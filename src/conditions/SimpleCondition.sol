360a853a67fc4c763ae7bb1817300277a967bfa7
Aditya <adityam874@gmail.com>
2025-05-16 14:47:30 +0530

    conditions/SimpleCondition.sol file added

---
+++ src/conditions/SimpleCondition.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "../interfaces/IAzuranceCondition.sol";
+import "../interfaces/IAzurancePool.sol";
+
+contract SimpleCondition is IAzuranceCondition {
+
+    function checkUnlockClaim(address target) external override {
+        IAzurancePool(target).unlockClaim();
+    }
+
+    function checkUnlockTerminate(address target) external override {
+        IAzurancePool(target).unlockTerminate();
+    }
+}
