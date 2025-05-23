27dbba5755faa4e3fdb59fd2cb87467b2e24031d
Aditya <adityam874@gmail.com>
2025-05-23 12:01:06 +0530

    all interfaces added

---
+++ src/interfaces/IAzuranceFactory.sol
@@ -0,0 +1,16 @@
+// SPDX--License-Identifier: MIT
+pragma solidity >=0.5.0;
+
+interface IAzuranceFactory {
+    function createAzuranceContract (
+        uint256 multiplier_,
+        uint256 multiplierBlocks_,
+        uint256 staleBlocks_,
+        address asset_,
+        uint256 fee_,
+        address feeTo_,
+        address condition_,
+        string memory name_,
+        string memory symbol_
+    ) external returns (address);
+}
---
+++ src/interfaces/ICovidFunction.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+interface ICovidFunction {
+    function acceptOwnership() external;
+
+    function addToWhitelist(address _address) external;
+
+    error EmptyArgs();
+    error EmptySource();
+
+    function handleOracleFulfillment(
+        bytes32 requestId,
+        bytes memory response,
+        bytes memory err
+    ) external;
+
+    error NoInlineSecrets();
+    error OnlyRouterCanFulfill();
+
+    function removeFromWhitelist(address _address) external;
+
+    error UnexpectedRequestID(bytes32 requestId);
+    event OwnershipTransferRequested(address indexed from, address indexed to);
+    event OwnershipTransferred(address indexed from, address indexed to);
+    event RequestFulfilled(bytes32 indexed id);
+    event RequestSent(bytes32 indexed id);
+    event Response(
+        bytes32 indexed requestId,
+        string country,
+        bytes response,
+        bytes err
+    );
+
+    function sendRequest(uint64 subscriptionId, string[] memory args) external returns (bytes32 requestId);
+
+    function transferOwnership(address to) external;
+
+    function countryCovidPercentage(string memory) external view returns (bool);
+
+    function listOfRequestIdxCountry(bytes32) external view returns (string memory);
+
+    function owner() external view returns (address);
+
+    function s_lastError() external view returns (bytes memory);
+
+    function s_lastRequestId() external view returns (bytes32);
+
+    function s_lastResponse() external view returns (bytes memory);
+
+    function whitelist(address) external view returns (bool);
+}
