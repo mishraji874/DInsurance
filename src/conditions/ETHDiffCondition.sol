8e0f0cb90dba7b4ff85318947c38c6da5c3ae78c
Aditya <adityam874@gmail.com>
2025-05-23 13:17:07 +0530

    lib updated and LendingLockCondition.sol and ETHDiffCondition.sol created

---
+++ src/conditions/ETHDiffCondition.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "../interfaces/IAzuranceCondition.sol";
+import "../interfaces/IAzurancePool.sol";
+import "../interfaces/IETHPriceFeed.sol";
+import "../interfaces/ICovidFunction.sol";
+
+contract ETHDiffCondition is IAzuranceCondition {
+    IETHPriceFeed public dataFeed;
+
+    constructor(address _dataFeed) {
+        dataFeed = IETHPriceFeed(_dataFeed);
+    }
+
+    function checkUnlockClaim(address target) external override {
+        // eth diff -50%
+        if(dataFeed.pricediff() < -50) {
+            IAzurancePool(target).unlockClaim();
+        }
+    }
+
+    function checkUnlockTerminate(address target) external override {
+        IAzurancePool(target).unlockTerminate();
+    }
+}
