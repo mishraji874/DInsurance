75928e0d64b4d834590dc446cb930ccbe092d471
Aditya <adityam874@gmail.com>
2025-05-24 11:40:24 +0530

    all conditions implemented

---
+++ src/conditions/CovidConditon.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
+import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
+
+import "../interfaces/IAzuranceCondition.sol";
+import "../interfaces/IAzurancePool.sol";
+
+contract CovidCondition is IAzuranceCondition, FunctionsClient, ConfirmedOwner {
+    using FunctionsRequest for FunctionsRequest.Request;
+
+    bytes32 public s_lastRequestId;
+    bytes public s_lastResponse;
+    bytes public s_lastError;
+
+    // Check to get the router address for your supported network https://docs.chain.link/chainlink-functions/supported-networks
+    address router = 0xb83E47C2bC239B3bf370bc41e1459A34b41238D0;
+
+    // donID - Hardcoded for Fuji
+    // Check to get the donID for your supported network https://docs.chain.link/chainlink-functions/supported-networks
+    bytes32 donID =
+        0x66756e2d657468657265756d2d7365706f6c69612d3100000000000000000000;
+    uint32 gasLimit = 300000;
+
+    uint64 subscriptionId;
+    mapping(bytes32 => address) public requestTarget;
+
+    string source =
+        "const country = args[0];"
+        "const apiResponse = await Functions.makeHttpRequest({"
+        "url: `https://disease.sh/v3/covid-19/countries/${country}?strict=true`"
+        "});"
+        "if (apiResponse.error) {"
+        "throw Error(`Request failed`);"
+        "}"
+        "const { data } = apiResponse;"
+        "const percentage = Math.floor((data.active*Math.pow(10,18)/data.population));"
+        "return Functions.encodeUint256(percentage);";
+
+    error UnexpectedRequestID(bytes32 requestId);
+
+    event Response(
+        bytes32 indexed requestId,
+        address target,
+        bytes response,
+        bytes err
+    );
+
+    constructor(uint64 subscriptionId_) FunctionsClient(router) ConfirmedOwner(msg.sender) {
+        subscriptionId = subscriptionId_;
+    }
+    
+    function checkUnlockClaim(address target) external override {
+        require(msg.sender == target, "The contract must check itselfs");
+
+        FunctionsRequest.Request memory req;
+        req.initializeRequestForInlineJavaScript(source); 
+
+        string[] memory args = new string[](1);
+        args[0] = "th";
+        req.setArgs(args);
+
+        s_lastRequestId = _sendRequest(
+            req.encodeCBOR(),
+            subscriptionId,
+            gasLimit,
+            donID
+        );
+
+        requestTarget[s_lastRequestId] = target;
+    }
+
+    function checkUnlockTerminate(address target) external override {
+        // For demo purpose only. Please add a strict condition on production
+        IAzurancePool(target).unlockTerminate();
+    }
+
+    function fulfillRequest(
+        bytes32 requestId,
+        bytes memory response,
+        bytes memory err
+    ) internal override {
+        if (s_lastRequestId != requestId) {
+            revert UnexpectedRequestID(requestId);
+        }
+        s_lastResponse = response;
+        s_lastError = err;
+
+        uint percentage = uint256(bytes32(response));
+        address target = requestTarget[requestId];
+
+        if (percentage >= 30 * 10**18) {
+            IAzurancePool(target).unlockClaim();
+        }
+
+        // Emit an event to log the response
+        emit Response(requestId, target, s_lastResponse, s_lastError);
+    }
+}
---
+++ src/conditions/ETHCrashCondition.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "../interfaces/IAzuranceCondition.sol";
+import "../interfaces/IAzurancePool.sol";
+
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract ETHCrashCondition is IAzuranceCondition {
+    AggregatorV3Interface internal dataFeed;
+
+    constructor() {
+        // fixed aggregator in sepolia chain
+        dataFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+    }
+
+    function getPrice() public view returns (int256) {
+        (
+            /* uint80 roundID */,
+            int256 answer,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = dataFeed.latestRoundData();
+        return answer;
+    }
+
+    function isEligible() public view returns (bool) {
+        int256 price = getPrice();
+        return price < 2500 * (10**8);
+    }
+
+    function checkUnlockClaim(address target) external override {
+        require(msg.sender == target, "Only target can check itself");
+        if(isEligible()) {
+            IAzurancePool(target).unlockClaim();
+        }
+    }
+
+    function checkUnlockTerminate(address target) external override {
+        // for demo purpose only. do check the strict condition on production
+        IAzurancePool(target).unlockTerminate();
+    }
+}
