3aa1dd756695057a2648e44fbe24cf549d1c76e8
Aditya <adityam874@gmail.com>
2025-05-26 10:48:34 +0530

    test/contracts updated

---
+++ test/contracts/TestERC4626.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+
+contract TestERC4626 is ERC4626 {
+    constructor(address asset_) ERC4626(IERC20(asset_)) ERC20("TestYieldToken", "TYT") {}
+}
